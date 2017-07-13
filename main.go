dfbd8c2d6b5ace03f929ffa6b94b7399b212dfa3
pallat <yod.pallat@gmail.com>
2017-07-13 21:40:48 +0700

    copy code from internet

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -28,8 +33,43 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/oauth/callback", callback)
+	e.GET("/", callback)
+	e.GET("/oauth/callback", handleGoogleCallback)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
+}
+
+var (
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:3000/GoogleCallback",
+		ClientID:     os.Getenv("googlekey"),
+		ClientSecret: os.Getenv("googlesecret"),
+		Scopes: []string{"https://www.googleapis.com/auth/userinfo.profile",
+			"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint: google.Endpoint,
+	}
+	// Some random string, random for each request
+	oauthStateString = "random"
+)
+
+func handleGoogleCallback(c echo.Context) error {
+	state := c.QueryParam("state")
+	if state != oauthStateString {
+		fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
+		return c.Redirect(http.StatusTemporaryRedirect, "/")
+	}
+
+	code := c.QueryParam("code")
+	token, err := googleOauthConfig.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		fmt.Printf("Code exchange failed with '%s'\n", err)
+		return c.Redirect(http.StatusTemporaryRedirect, "/")
+	}
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	return c.String(200, "Content: %s\n", contents)
 }
